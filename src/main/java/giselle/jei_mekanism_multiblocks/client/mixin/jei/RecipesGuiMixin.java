5de7a7aadb14fb48f866799afeaab1b74ea100e3
Giselle <gisellevonbingen@gmail.com>
2024-07-17 09:22:32 +0900

    Fix #12

--- src/main/java/giselle/jei_mekanism_multiblocks/client/mixin/jei/RecipesGuiMixin.java
+++ src/main/java/giselle/jei_mekanism_multiblocks/client/mixin/jei/RecipesGuiMixin.java
@@ -9,32 +9,39 @@ import org.spongepowered.asm.mixin.Shadow;
 import giselle.jei_mekanism_multiblocks.client.IRecipeLayoutHolder;
 import giselle.jei_mekanism_multiblocks.client.IRecipeLogicStateListener;
 import mezz.jei.api.gui.IRecipeLayoutDrawable;
+import mezz.jei.gui.recipes.RecipeGuiLayouts;
+import mezz.jei.gui.recipes.RecipeLayoutWithButtons;
 import mezz.jei.gui.recipes.RecipesGui;
-import mezz.jei.library.gui.recipes.RecipeLayout;
 
 @Mixin(value = RecipesGui.class, remap = false)
-public class RecipesGuiMixin implements IRecipeLayoutHolder, IRecipeLogicStateListener
+public abstract class RecipesGuiMixin implements IRecipeLayoutHolder, IRecipeLogicStateListener
 {
 	@Shadow
-	private final List<RecipeLayout<?>> recipeLayouts = new ArrayList<>();
+	private RecipeGuiLayouts layouts;
 
 	@Override
 	public List<IRecipeLayoutDrawable<?>> jei_mekanism_multiblocks$getRecipeLayouts()
 	{
 		List<IRecipeLayoutDrawable<?>> list = new ArrayList<>();
 
-		for (RecipeLayout<?> recipeLayout : this.recipeLayouts)
+		for (RecipeLayoutWithButtons<?> recipeLayoutWithButtons : ((RecipeGuiLayoutsAccessor) this.layouts).getRecipeLayoutsWithButtons())
 		{
-			list.add(recipeLayout);
+			list.add(recipeLayoutWithButtons.getRecipeLayout());
 		}
 
 		return list;
 	}
 
+	@Shadow
+	private void updateLayout()
+	{
+
+	}
+
 	@Override
 	public void jei_mekanism_multiblocks$onStateChange()
 	{
-		((RecipesGui) (Object) (this)).onStateChange();
+		this.updateLayout();
 	}
 
 }
